58b4dcc4932231f707b57c18073d83d9672789ae
Brian Bixby <brianbixby0@gmail.com>
2018-01-22 11:48:07 -0800

    stuck

--- jasper-brian/public/scripts/article.js
+++ jasper-brian/public/scripts/article.js
@@ -43,9 +43,10 @@ var app = app || {};
 
   Article.numWordsAll = () => {
     console.log('article.numwordsall');
+    console.log(Article.all.map().reduce());
     return {
       author: `${author}`,
-      wordCount: Article.all.map().reduce()
+      wordCount: Article.all.map().reduce(),
     }
   };
 
@@ -56,8 +57,21 @@ var app = app || {};
 
   Article.numWordsByAuthor = () => {
     console.log('article.numwordsbyauthor');
-    return Article.allAuthors().map(author => { })
+    return {
+      author: `${author}`,
+      wordCount: Article.allAuthors().map(author => { }),
+    } //
   };
+  // let newArr = people.reduce((acc, curr) => {
+  //   if (curr.id > 10) {
+  //     acc.push({
+  //       id: curr.id,
+  //       name: curr.name.split(' '),
+  //       diffBornAndGrad: curr.graduated - curr.born
+  //     })
+  //   }
+  //   return acc
+  // }, [])
 
   Article.truncateTable = callback => {
     console.log('article.truncatetable');
--- jasper-brian/public/scripts/articleView.js
+++ jasper-brian/public/scripts/articleView.js
@@ -144,7 +144,7 @@ var app = app || {};
   articleView.initAdminPage = () => {
     console.log('articleview.initadminpage');
     // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM. The callback is not required to return anything.
-    let template = Handlebars.compile($('#author-stats-template').text());
+    let template = Handlebars.compile($('#author-stats-template').html());
     app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
 
     // REVIEW: Simply write the correct values to the page:
